5e5f0d6aba9b915a67eec2b63d2820c185ded88b
Mike <61601920+Telea-Mihai@users.noreply.github.com>
2025-07-13 22:42:29 +0300

    Switched to monorepo

--- WEB/2n2d/src/db/schemas/user.ts
+++
@@ -1,9 +0,0 @@
-import {pgTable, serial, text, integer} from "drizzle-orm/pg-core"
-
-export const user = pgTable("user", {
-    id: serial("id").primaryKey().notNull(),
-    uid: text("uid").notNull(),
-    email: text("email").notNull(),
-    displayName: text("displayname"),
-    sessions: integer("sessions").array()
-})
